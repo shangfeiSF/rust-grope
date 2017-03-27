76fd48e367f92888b93d5e7740773b2974e3e0d2
shangfeiSF <tony20100125@126.com>
2017-03-27 17:53:50 +0800

    the base content about mutablilty

--- main/03_variable_bindings/src/main.rs
+++ main/03_variable_bindings/src/main.rs
@@ -8,11 +8,13 @@
 // 然后将这段内存区域与标识符a绑定，让标识符a成为这段内存区域的唯一所有者
 
 fn main() {
+    // （1）作用域
     {
         // Rust 通过花括号定义作用域
         // Rust 规定在离开作用域时，局部标识符随即会被销毁
         // Rust 不同于C++和Java，会将局部标识符绑定的内存区域，连同其所有者（局部标识符）一起被销毁释放
 
+        println!("#作用域");
         {
             let num: u32 = 5;
             println!("num:{:?}", num);
@@ -23,9 +25,13 @@ fn main() {
         // 这些错误都是在编译阶段发现的
         // println!("{:?}", num);
     }
+
+    // （2）绑定的初始值
     {
         // Rust 不会为标识符绑定默认的初始值
         // Rust 明确规定标识符绑定的初始值必须由程序员显式声明
+
+        println!("#绑定的初始值");
 
         let num_uninitialized: u32;
         // 因为 num_uninitialized 没有显式声明绑定的初始值，会报错
@@ -34,7 +40,11 @@ fn main() {
         num_uninitialized = 5;
         println!("num_uninitialized:{:?}", num_uninitialized);
     }
+
+    // （3）整型类型推断
     {
+        println!("#整型类型推断");
+
         // let 绑定整数，默认类型推断是 i32
         let num_1 = 5;
         let num_2: i32 = 5;
@@ -45,7 +55,11 @@ fn main() {
         // 因为类型不匹配，会报错
         // assert_eq!(num_1, num_3);
     }
+
+    // （4）浮点型类型推断
     {
+        println!("#浮点型类型推断");
+
         // let 绑定浮点数，默认类型推断是f32
         let num_1 = 2.0;
         let num_2: f32 = 2.0;
@@ -56,7 +70,11 @@ fn main() {
         // 因为类型不匹配，会报错
         // assert_eq!(num_1, num_3);
     }
+
+    // （5）值类型显式标记法
     {
+        println!("#值类型显式标记法");
+
         // Rust 值类型的显式标记法，Rust 语法规定为 value+type 的形式
         let num_1 = 5u32;
         let num_2 = 2.0f64;
@@ -70,7 +88,11 @@ fn main() {
         println!("num_1:{:?}, num_2:{:?}", num_1, num_2);
         println!("num_1:{:?}, num_2:{:?}", num_3, num_4);
     }
+
+    // （6）绑定和转移
     {
+        println!("#绑定和转移");
+
         // String 是一个带有的 vec:Vec<u8> 成员的结构体，你可以理解为 str 类型的动态形式
         let mut string_1 = String::new(); // String::new() 创建一个空的字符串
 
@@ -92,5 +114,95 @@ fn main() {
         println!("string_2:{:?}", string_2);
         // 被转移所有权的标识符不可以继续被使用，否则会造成被称为“转移后使用”的代码，而在编译阶段报错
         // println!("string_1:{:?}", string_1);
+    }
+
+    //  （7）Copy特性
+    {
+        // 实现 Copy 特性的数据类型，在 move 时会拷贝资源到新内存区域
+        // 并把新内存区域绑定到新的标识符上
+        // Rust 的基本数据类型(Primitive Types)均实现了Copy特性
+        // 包括i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64, (), bool, char等
+        println!("#Copy特性");
+
+        let num_1 = 100u32;
+        let num_2 = num_1;
+        println!("num_1:{}", num_1);
+        println!("num_2:{}", num_2);
+    }
+
+    // （8）浅拷贝和深拷贝
+    {
+        println!("#浅拷贝和深拷贝");
+
+        // 非基本类型String和基本类型u32，“深拷贝”（Clone）是一样的
+        let string_1 = String::from("Hello World!");
+        let num_1 = 10_10_10u32;
+        let string_2 = string_1.clone();
+        let num_2 = num_1.clone();
+
+        println!("string_1:{}", string_1);
+        println!("string_2:{}", string_2);
+        println!("num_1:{}", num_1);
+        println!("num_2:{}", num_2);
+
+        // 非基本类型String和基本类型u32的“转移”（Move），相对而言的“浅拷贝”是不一样的
+        let string_3 = string_1;
+        let num_3 = num_1;
+
+        // println!("string_1:{}", string_1);
+        println!("string_3:{}", string_3);
+        println!("num_1:{}", num_1);
+        println!("num_3:{}", num_3);
+    }
+
+    // （9）忽略未使用的绑定
+    {
+        println!("#忽略未使用的绑定");
+
+        let _unused = 5u32;
+
+        // 可以使用“_”开头的绑定来忽略 Rust 对未使用绑定的静态编译检查
+        // let noisy_unused = 5u32;
+    }
+
+    // （10）使用下划线改善数字的可读性
+    {
+        println!("#使用下划线改善数字的可读性");
+
+        let num_1 = 1_000_000u32;
+        let num_2 = 5.000_001f32;
+
+        println!("num_1:{:?}", num_1);
+        println!("num_2:{:?}", num_2);
+    }
+
+    // （11）声明进制，按特定进制格式输出
+    {
+        // 0b声明二进制，0o声明八进制，0x声明十六进制
+        let num_1 = 0b_1101_0101_u32;
+        let num_2 = 0o_45_67_u32;
+        let num_3 = 0x_a1_b2_u32;
+
+        // 格式输出时指定特殊的格式
+        // :0n 补零使位数为n位
+        // :b 按二进制格式输出，:o按八进制格式输出，:x按小写十六进制格式输出，:X按大写十六进制输出
+        // 特殊格式可以组合使用，例如:09X
+
+        println!("#声明进制，按特定进制格式输出");
+
+        println!("num_1:0b{:09b}", num_1);
+        println!("num_1:0o{:09o}", num_1);
+        println!("num_1:0x{:09x}", num_1);
+        println!("num_1:0x{:09X}", num_1);
+
+        println!("num_2:0b{:09b}", num_2);
+        println!("num_2:0o{:09o}", num_2);
+        println!("num_2:0x{:09x}", num_2);
+        println!("num_2:0x{:09X}", num_2);
+
+        println!("num_3:0b{:09b}", num_3);
+        println!("num_3:0o{:09o}", num_3);
+        println!("num_3:0x{:09x}", num_3);
+        println!("num_3:0x{:09X}", num_3);
     }
 }
