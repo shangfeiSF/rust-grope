a084ecc1de9c8df561de0990f333af33f9a45f0d
shangfeiSF <tony20100125@126.com>
2017-03-30 11:54:51 +0800

    learn about advanced_copy

--- main/04_mutability/examples/advanced_copy.rs
+++ main/04_mutability/examples/advanced_copy.rs
@@ -2,8 +2,8 @@
 #[derive(Debug)]
 
 struct Test {
-    num_1: f32,
-    bool_1: bool,
+    number: f32,
+    boolean: bool,
 }
 
 impl Copy for Test {}
@@ -11,8 +11,8 @@ impl Copy for Test {}
 impl Clone for Test {
     fn clone(&self) -> Test {
         Test {
-            num_1: self.num_1,
-            bool_1: self.bool_1,
+            number: self.number,
+            boolean: self.boolean,
         }
     }
 }
@@ -21,16 +21,16 @@ fn main() {
     println!("#手动实现 Copy/Clone特性");
 
     let x = Test {
-        num_1: 100.100_f32,
-        bool_1: true,
+        number: 100.100_f32,
+        boolean: true,
     };
 
     // Test 手动实现了 Copy特性和 Clone特性，所以
     // let mut y = x后，x并没有因为所有权move而出现不可访问错误
     let mut y = x;
 
-    y.num_1 = 200.200_f32;
-    y.bool_1 = false;
+    y.number = 200.200_f32;
+    y.boolean = false;
 
     println!("x:{:?}", x);
     println!("y:{:?}", y);
--- main/04_mutability/examples/advanced_copy_auto.rs
+++ main/04_mutability/examples/advanced_copy_auto.rs
@@ -1,23 +1,27 @@
 // 通过 derive 让 Rust 编译器自动实现 Copy/Clone特性
-#[derive(Copy, Clone, Debug)]
+#[derive(Debug)]
+#[derive(Copy, Clone)]
 
 struct Test {
-    num_1: f32,
-    bool_1: bool,
+    number: f32,
+    boolean: bool,
 }
 
 fn main() {
     println!("#通过 derive 让 Rust 编译器自动实现 Copy/Clone特性");
 
     let x = Test {
-        num_1: 100.100_f32,
-        bool_1: true,
+        number: 100.100_f32,
+        boolean: true,
     };
 
-    let y = x;
-
     // Test 通过 derive 自动继承了 Copy特性和 Clone特性，所以
-    // let y = x后，x并没有因为所有权move而出现不可访问错误
+    // let mut y = x后，x并没有因为所有权move而出现不可访问错误
+    let mut y = x;
+
+    y.number = 200.200_f32;
+    y.boolean = false;
+
     println!("x:{:?}", x);
     println!("y:{:?}", y);
 }
